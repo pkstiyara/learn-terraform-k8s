4db62bedee536e3c1c3dff8dcc410b86d5bc07c7
pkstiyara <pkstiyara@gmail.com>
2023-02-02 20:13:33 +0530

    Initial

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 vpc_name = "dev-vpc"
 instance_count = "2"
+instance_tags = "My-Instance"
--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,7 @@ module "vpc" {
   version = "3.19.0"
   name = var.vpc_name
   cidr = var.vpc_cidr
-
+  tags = var.vpc_tags
+  azs = var.vpc_azs
 
 }
